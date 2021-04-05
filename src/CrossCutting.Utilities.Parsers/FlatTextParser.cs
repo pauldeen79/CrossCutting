0a8a0d1a2871760418a8f40c4f4d6a264061a5f6
Paul Deen <paul.deen@dutchworkz.nl>
2021-04-05 08:32:50 +0200

    Added simple FlatTextParser

---
+++ src/CrossCutting.Utilities.Parsers/FlatTextParser.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace CrossCutting.Utilities.Parsers
+{
+    public static class FlatTextParser
+    {
+        public static string[] Parse(string instance, char delimiter, char? textQualifier = null)
+        {
+            var result = new List<string>();
+            var currentSection = new StringBuilder();
+            var inText = false;
+            foreach (var character in instance)
+            {
+                if (character == delimiter && !inText)
+                {
+                    result.Add(currentSection.ToString());
+                    currentSection.Clear();
+                }
+                else if (textQualifier != null && character == textQualifier)
+                {
+                    // skip this character
+                    inText = !inText;
+                }
+                else
+                {
+                    currentSection.Append(character);
+                }
+            }
+
+            if (currentSection.Length > 0)
+            {
+                result.Add(currentSection.ToString());
+            }
+
+            return result.ToArray();
+        }
+    }
+}
