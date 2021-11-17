27c570115d24d29d53d5ba7f84a76517d020983a
Paul Deen <paul.deen@dutchworkz.nl>
2021-11-17 19:47:31 +0100

    Removed useless project

--- src/CrossCutting.Utilities.XmlDocumentTransformation/Interfaces/IXDocumentTransformComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Xml.Linq;
-
-namespace CrossCutting.Utilities.XmlDocumentTransformation.Interfaces
-{
-    public interface IXDocumentTransformComponent<in T>
-    {
-        void Transform(XDocument result, T parameters);
-    }
-}
--- src/CrossCutting.Utilities.XmlDocumentTransformation/Interfaces/IXmlDocumentTransformComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Xml;
-
-namespace CrossCutting.Utilities.XmlDocumentTransformation.Interfaces
-{
-    public interface IXmlDocumentTransformComponent<in T>
-    {
-        void Transform(XmlDocument result, T parameters);
-    }
-}
