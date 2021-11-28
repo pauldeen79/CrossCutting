8afafe347d3474b0488d4f4f1b96147a82257ff0
Paul Deen <paul.deen@dutchworkz.nl>
2021-11-28 12:04:45 +0100

    Added builder for UPDATE commands

---
+++ src/CrossCutting.Data.Core.Tests/Builders/UpdateCommandBuilderTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using CrossCutting.Data.Core.Builders;
+using FluentAssertions;
+using Xunit;
+
+namespace CrossCutting.Data.Core.Tests.Builders
+{
+    [ExcludeFromCodeCoverage]
+    public class UpdateCommandBuilderTests
+    {
+        [Fact]
+        public void Build_Throws_When_TableName_Is_Missing()
+        {
+            // Arrange
+            var input = new UpdateCommandBuilder();
+
+            // Act & Assert
+            input.Invoking(x => x.Build())
+                 .Should().Throw<InvalidOperationException>()
+                 .WithMessage("table name is missing");
+        }
+
+        [Fact]
+        public void Build_Throws_When_FieldNames_Are_Empty()
+        {
+            // Arrange
+            var input = new UpdateCommandBuilder().Table("MyTable");
+
+            // Act & Assert
+            input.Invoking(x => x.Build())
+                 .Should().Throw<InvalidOperationException>()
+                 .WithMessage("field names are missing");
+        }
+
+        [Fact]
+        public void Build_Throws_When_FieldValues_Are_Empty()
+        {
+            // Arrange
+            var input = new UpdateCommandBuilder().Table("MyTable").WithFieldNames("Field1", "Field2", "Field3");
+
+            // Act & Assert
+            input.Invoking(x => x.Build())
+                 .Should().Throw<InvalidOperationException>()
+                 .WithMessage("field values are missing");
+        }
+
+        [Fact]
+        public void Build_Throws_When_FieldNames_And_FieldValues_Count_Are_Not_Equal()
+        {
+            // Arrange
+            var input = new UpdateCommandBuilder().Table("MyTable")
+                .WithFieldNames("Field1", "Field2", "Field3")
+                .WithFieldValues("Value1", "Value2");
+
+            // Act & Assert
+            input.Invoking(x => x.Build())
+                 .Should().Throw<InvalidOperationException>()
+                 .WithMessage("field name count should be equal to field value count");
+        }
+
+        [Fact]
+        public void Build_Generates_Command_With_Parameters()
+        {
+            // Arrange
+            var input = new UpdateCommandBuilder().Table("MyTable")
+                .WithFieldNames("Field1", "Field2", "Field3")
+                .WithFieldValues("@Field1", "@Field2", "@Field3")
+                .AppendParameters(new { Field1 = "Value1", Field2 = "Value2", Field3 = "Value3" });
+
+            // Act
+            var actual = input.Build();
+
+            // Assert
+            actual.Operation.Should().Be(Abstractions.DatabaseOperation.Update);
+            actual.CommandText.Should().Be("UPDATE MyTable SET Field1 = @Field1, Field2 = @Field2, Field3 = @Field3");
+            actual.CommandParameters.Should().BeAssignableTo<IDictionary<string, object>>();
+            var parameters = actual.CommandParameters as IDictionary<string, object>;
+            if (parameters != null)
+            {
+                parameters.Should().HaveCount(3);
+                parameters.Keys.Should().BeEquivalentTo(new[] { "Field1", "Field2", "Field3" });
+                parameters.Values.Should().BeEquivalentTo(new[] { "Value1", "Value2", "Value3" });
+            }
+        }
+
+        [Fact]
+        public void Build_Generates_Command_With_Where_Statement()
+        {
+            // Arrange
+            var input = new UpdateCommandBuilder().Table("MyTable")
+                .WithFieldNames("Field1", "Field2", "Field3")
+                .WithFieldValues("\"Field1\"", "\"Field2\"", "\"Field3\"")
+                .Where("Field1 = \"OldValue1\"")
+                .And("Field2 = \"OldValue2\"")
+                .And("Field3 = \"OldValue3\"");
+
+            // Act
+            var actual = input.Build();
+
+            // Assert
+            actual.Operation.Should().Be(Abstractions.DatabaseOperation.Update);
+            actual.CommandText.Should().Be("UPDATE MyTable SET Field1 = \"Field1\", Field2 = \"Field2\", Field3 = \"Field3\" WHERE Field1 = \"OldValue1\" AND Field2 = \"OldValue2\" AND Field3 = \"OldValue3\"");
+            actual.CommandParameters.Should().BeAssignableTo<IDictionary<string, object>>();
+            var parameters = actual.CommandParameters as IDictionary<string, object>;
+            parameters.Should().BeEmpty();
+        }
+    }
+}
---
+++ src/CrossCutting.Data.Core/Builders/UpdateCommandBuilder.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CrossCutting.Common.Extensions;
+using CrossCutting.Data.Abstractions;
+using CrossCutting.Data.Core.Commands;
+
+namespace CrossCutting.Data.Core.Builders
+{
+    public class UpdateCommandBuilder
+    {
+        public DatabaseCommandType CommandType { get; set; }
+        public IDictionary<string, object> CommandParameters { get; set; }
+        private string _table;
+        private readonly List<string> _fieldNames;
+        private readonly List<string> _fieldValues;
+        private readonly StringBuilder _whereBuilder;
+
+        public UpdateCommandBuilder()
+        {
+            CommandParameters = new Dictionary<string, object>();
+            _table = string.Empty;
+            _fieldNames = new List<string>();
+            _fieldValues = new List<string>();
+            _whereBuilder = new StringBuilder();
+        }
+
+        public UpdateCommandBuilder Table(string table)
+        {
+            _table = table;
+            return this;
+        }
+
+        public UpdateCommandBuilder WithFieldName(string fieldName)
+        {
+            _fieldNames.Add(fieldName);
+            return this;
+        }
+
+        public UpdateCommandBuilder WithFieldNames(IEnumerable<string> fieldNames)
+        {
+            _fieldNames.AddRange(fieldNames);
+            return this;
+        }
+
+        public UpdateCommandBuilder WithFieldNames(params string[] fieldNames)
+        {
+            _fieldNames.AddRange(fieldNames);
+            return this;
+        }
+
+        public UpdateCommandBuilder WithFieldValue(string fieldValue)
+        {
+            _fieldValues.Add(fieldValue);
+            return this;
+        }
+
+        public UpdateCommandBuilder WithFieldValues(IEnumerable<string> fieldValues)
+        {
+            _fieldValues.AddRange(fieldValues);
+            return this;
+        }
+
+        public UpdateCommandBuilder WithFieldValues(params string[] fieldValues)
+        {
+            _fieldValues.AddRange(fieldValues);
+            return this;
+        }
+
+        public UpdateCommandBuilder Where(string value)
+        {
+            if (_whereBuilder.Length > 0)
+            {
+                _whereBuilder.Append(" AND ");
+            }
+            _whereBuilder.Append(value);
+            return this;
+        }
+
+        public UpdateCommandBuilder And(string value)
+            => Where(value);
+
+        public UpdateCommandBuilder Or(string value)
+        {
+            if (_whereBuilder.Length == 0)
+            {
+                throw new InvalidOperationException("There is no WHERE clause to combine the current value with");
+            }
+            _whereBuilder.Append(" OR ").Append(value);
+            return this;
+        }
+
+        public UpdateCommandBuilder AppendParameter(string key, object value)
+        {
+            CommandParameters.Add(key, value);
+            return this;
+        }
+
+        public UpdateCommandBuilder AppendParameters(object parameters)
+        {
+            foreach (var param in parameters.ToExpandoObject())
+            {
+                CommandParameters.Add(param.Key, param.Value);
+            }
+            return this;
+        }
+
+        public UpdateCommandBuilder Clear()
+        {
+            CommandParameters.Clear();
+            _table = string.Empty;
+            _fieldNames.Clear();
+            _fieldValues.Clear();
+            _whereBuilder.Clear();
+            return this;
+        }
+
+        public UpdateCommandBuilder AsStoredProcedure()
+        {
+            CommandType = DatabaseCommandType.StoredProcedure;
+            return this;
+        }
+
+        public UpdateCommandBuilder AsText()
+        {
+            CommandType = DatabaseCommandType.Text;
+            return this;
+        }
+
+        public IDatabaseCommand Build()
+            => new SqlDatabaseCommand(BuildSql(), CommandType, DatabaseOperation.Update, CommandParameters);
+
+        private string BuildSql()
+        {
+            if (string.IsNullOrWhiteSpace(_table))
+            {
+                throw new InvalidOperationException("table name is missing");
+            }
+
+            if (_fieldNames.Count == 0)
+            {
+                throw new InvalidOperationException("field names are missing");
+            }
+
+            if (_fieldValues.Count == 0)
+            {
+                throw new InvalidOperationException("field values are missing");
+            }
+
+            if (_fieldNames.Count != _fieldValues.Count)
+            {
+                throw new InvalidOperationException("field name count should be equal to field value count");
+            }
+
+            var builder = new StringBuilder()
+                .Append("UPDATE ")
+                .Append(_table)
+                .Append(" SET ")
+                .Append(string.Join(", ", GenerateValues()));
+
+            if (_whereBuilder.Length > 0)
+            {
+                builder.Append(" WHERE ").Append(_whereBuilder);
+            }
+
+            return builder.ToString();
+        }
+
+        private IEnumerable<string> GenerateValues()
+            => _fieldNames.Zip(_fieldValues, (name, value) => $"{name} = {value}");
+    }
+}
