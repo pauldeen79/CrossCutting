b23a08d97db5e5c7cb47f4c23d7bff46e10e8526
Paul Deen <30937073+pauldeen79@users.noreply.github.com>
2022-05-27 09:16:53 +0200

    ReadOnlyValueCollection (#23)
    * Minor: Upgraded Sonar ruleset to latest version
    
    * Minor: Upgraded FluentAssertions and Moq to latest version
    
    * Added ReadOnlyValueCollection implementation
    
    * Upgraded test package references
    
    * Fix build pipeline
    
    * Removed some code duplication

--- src/CrossCutting.Common/ReadOnlyValueCollection.cs
+++ src/CrossCutting.Common/ReadOnlyValueCollection.cs
@@ -1,25 +1,25 @@
 ﻿namespace CrossCutting.Common;
 
-public sealed class ValueCollection<T> : Collection<T>, IEquatable<ValueCollection<T>>, IFormattable
+public sealed class ReadOnlyValueCollection<T> : ReadOnlyCollection<T>, IEquatable<ReadOnlyValueCollection<T>>, IFormattable
 {
     private readonly IEqualityComparer<T> _equalityComparer;
 
-    public ValueCollection() : this(Enumerable.Empty<T>())
+    public ReadOnlyValueCollection() : this(Enumerable.Empty<T>())
     {
     }
 
-    public ValueCollection(IEqualityComparer<T>? equalityComparer) : this(Enumerable.Empty<T>(), equalityComparer)
+    public ReadOnlyValueCollection(IEqualityComparer<T>? equalityComparer) : this(Enumerable.Empty<T>(), equalityComparer)
     {
     }
 
-    public ValueCollection(IEnumerable<T> list, IEqualityComparer<T>? equalityComparer = null) : base(list.ToList())
+    public ReadOnlyValueCollection(IEnumerable<T> list, IEqualityComparer<T>? equalityComparer = null) : base(list.ToList())
         => _equalityComparer = equalityComparer ?? EqualityComparer<T>.Default;
 
-    public bool Equals(ValueCollection<T>? other)
+    public bool Equals(ReadOnlyValueCollection<T>? other)
         => ValueCollectionBase.Equals(this, other, _equalityComparer);
 
     public override bool Equals(object? obj)
-        => obj is { } && (ReferenceEquals(this, obj) || obj is ValueCollection<T> coll && Equals(coll));
+        => obj is { } && (ReferenceEquals(this, obj) || obj is ReadOnlyValueCollection<T> coll && Equals(coll));
 
     public override int GetHashCode()
         => unchecked(Items.Aggregate(0,
