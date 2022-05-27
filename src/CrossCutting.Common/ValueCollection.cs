6d8cd261f3cd1c22f333709691b7cd627eb526e1
Paul Deen <paul.deen@dutchworkz.nl>
2022-05-27 09:10:58 +0200

    Removed some code duplication

--- src/CrossCutting.Common/ValueCollection.cs
+++ src/CrossCutting.Common/ValueCollection.cs
@@ -16,30 +16,7 @@ public sealed class ValueCollection<T> : Collection<T>, IEquatable<ValueCollecti
         => _equalityComparer = equalityComparer ?? EqualityComparer<T>.Default;
 
     public bool Equals(ValueCollection<T>? other)
-    {
-        if (other is null)
-        {
-            return false;
-        }
-
-        if (ReferenceEquals(this, other))
-        {
-            return true;
-        }
-
-        using var enumerator1 = GetEnumerator();
-        using var enumerator2 = other.GetEnumerator();
-
-        while (enumerator1.MoveNext())
-        {
-            if (!enumerator2.MoveNext() || !(_equalityComparer!).Equals(enumerator1.Current, enumerator2.Current))
-            {
-                return false;
-            }
-        }
-
-        return !enumerator2.MoveNext(); //both enumerations reached the end
-    }
+        => ValueCollectionBase.Equals(this, other, _equalityComparer);
 
     public override bool Equals(object? obj)
         => obj is { } && (ReferenceEquals(this, obj) || obj is ValueCollection<T> coll && Equals(coll));
@@ -50,20 +27,7 @@ public sealed class ValueCollection<T> : Collection<T>, IEquatable<ValueCollecti
         ));
 
     public string ToString(string? format, IFormatProvider? formatProvider)
-        => "[" + string.Join(", ", this.Select(e => FormatValue(e, formatProvider))) + "]";
+        => "[" + string.Join(", ", this.Select(e => ValueCollectionBase.FormatValue(e, formatProvider))) + "]";
 
     public override string ToString() => ToString(null, CultureInfo.CurrentCulture);
-
-    private static string? FormatValue(object? value, IFormatProvider? formatProvider)
-        => value switch
-        {
-            null => "∅",
-            bool b => b ? "true" : "false",
-            string s => $"\"{s}\"",
-            char c => $"\'{c}\'",
-            DateTime dt => dt.ToString("o", formatProvider),
-            IFormattable @if => @if.ToString(null, formatProvider),
-            IEnumerable ie => "[" + string.Join(", ", ie.Cast<object>().Select(e => FormatValue(e, formatProvider))) + "]",
-            _ => value.ToString()
-        };
 }
---
+++ src/CrossCutting.Common/ValueCollectionBase.cs
@@ -0,0 +1,43 @@
+﻿namespace CrossCutting.Common;
+
+internal static class ValueCollectionBase
+{
+    internal static string? FormatValue(object? value, IFormatProvider? formatProvider)
+        => value switch
+        {
+            null => "∅",
+            bool b => b ? "true" : "false",
+            string s => $"\"{s}\"",
+            char c => $"\'{c}\'",
+            DateTime dt => dt.ToString("o", formatProvider),
+            IFormattable @if => @if.ToString(null, formatProvider),
+            IEnumerable ie => "[" + string.Join(", ", ie.Cast<object>().Select(e => FormatValue(e, formatProvider))) + "]",
+            _ => value.ToString()
+        };
+
+    internal static bool Equals<T>(IEnumerable<T> instance, IEnumerable<T>? other, IEqualityComparer<T> equalityComparer)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(instance, other))
+        {
+            return true;
+        }
+
+        using var enumerator1 = instance.GetEnumerator();
+        using var enumerator2 = other.GetEnumerator();
+
+        while (enumerator1.MoveNext())
+        {
+            if (!enumerator2.MoveNext() || !(equalityComparer!).Equals(enumerator1.Current, enumerator2.Current))
+            {
+                return false;
+            }
+        }
+
+        return !enumerator2.MoveNext(); //both enumerations reached the end
+    }
+}
