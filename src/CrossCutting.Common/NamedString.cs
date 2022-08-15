92d07510a6d5c1e928b208003778838c68923a42
Paul Deen <paul.deen@dutchworkz.nl>
2022-08-15 21:12:11 +0200

    Added NamedString.Format function to get rid of those nasty format strings with numbered arguments, which make no sense when they're part of your public API

---
+++ src/CrossCutting.Common/NamedString.cs
@@ -0,0 +1,19 @@
+﻿namespace CrossCutting.Common;
+
+public static class NamedString
+{
+    public static string Format(string template, object parameters)
+        => Format(template, parameters, false);
+
+    public static string Format(string template, object parameters, bool ignoreCase)
+    {
+        var parametersDictionary = parameters.ToExpandoObject() as IDictionary<string, object>;
+
+        foreach (var item in parametersDictionary)
+        {
+            template = Regex.Replace(template, $"{{{item.Key}}}", item.Value.ToStringWithDefault(), ignoreCase ? RegexOptions.IgnoreCase : RegexOptions.None);
+        }
+
+        return template;
+    }
+}
