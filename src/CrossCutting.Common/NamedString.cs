2240694163404ea28da1504d901bad461219c972
Paul Deen <30937073+pauldeen79@users.noreply.github.com>
2022-08-15 21:23:01 +0200

    Named string format (#24)
    * Added NamedString.Format function to get rid of those nasty format strings with numbered arguments, which make no sense when they're part of your public API
    
    * Refactored NamedString.Format to single LINQ Aggregate statement, with one helper method to convert boolean to RegexOptions

---
+++ src/CrossCutting.Common/NamedString.cs
@@ -0,0 +1,19 @@
+﻿namespace CrossCutting.Common;
+
+public static class NamedString
+{
+    public static string Format(string template, object parameters)
+        => Format(template, parameters, false);
+
+    public static string Format(string template, object parameters, bool ignoreCase)
+        => parameters.ToExpandoObject().Aggregate
+        (
+            template,
+            (previous, item) => Regex.Replace(previous, $"{{{item.Key}}}", item.Value.ToStringWithDefault(), CreateRegExOptions(ignoreCase))
+        );
+
+    private static RegexOptions CreateRegExOptions(bool ignoreCase)
+        => ignoreCase
+            ? RegexOptions.IgnoreCase
+            : RegexOptions.None;
+}
