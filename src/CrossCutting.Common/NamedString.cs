eeef8e56c7836bcfb983c7c35397ea0dd7d3d385
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-09 16:26:48 +0200

    Fixed second security hotspot regarding regex operation without time-out

--- src/CrossCutting.Common/NamedString.cs
+++ src/CrossCutting.Common/NamedString.cs
@@ -3,13 +3,16 @@
 public static class NamedString
 {
     public static string Format(string template, object parameters)
-        => Format(template, parameters, false);
+        => Format(template, parameters, false, 500);
 
     public static string Format(string template, object parameters, bool ignoreCase)
+        => Format(template, parameters, ignoreCase, 500);
+
+    public static string Format(string template, object parameters, bool ignoreCase, int matchTimeoutInMilliseconds)
         => parameters.ToExpandoObject().Aggregate
         (
             template,
-            (previous, item) => Regex.Replace(previous, $"{{{item.Key}}}", item.Value.ToStringWithDefault(), CreateRegExOptions(ignoreCase))
+            (previous, item) => Regex.Replace(previous, $"{{{item.Key}}}", item.Value.ToStringWithDefault(), CreateRegExOptions(ignoreCase), TimeSpan.FromMilliseconds(matchTimeoutInMilliseconds))
         );
 
     private static RegexOptions CreateRegExOptions(bool ignoreCase)
