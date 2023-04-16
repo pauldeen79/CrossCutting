fae5cd61a64e3efd5e62d12c44c1befd07147ede
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-16 12:13:27 +0200

    Added missing unit tests

--- src/CrossCutting.Utilities.FunctionParser.Tests/Extensions/StringExtensionsTests.cs
+++ src/CrossCutting.Utilities.FunctionParser.Tests/Extensions/StringExtensionsTests.cs
@@ -3,7 +3,7 @@
 public class StringExtensionsTests
 {
     [Fact]
-    public void Can_SafeSplit_StringValue()
+    public void Can_SafeSplit_StringValue_With_Equal_Open_And_Close_Token()
     {
         // Arrange
         var input = "a,'b,c',d";
@@ -16,5 +16,37 @@ public class StringExtensionsTests
         actual.ElementAt(0).Should().Be("a");
         actual.ElementAt(1).Should().Be("b,c");
         actual.ElementAt(2).Should().Be("d");
+    }
+
+    [Fact]
+    public void Can_SafeSplit_StringValue_With_Different_Open_And_Close_Token()
+    {
+        // Arrange
+        var input = "a,[b,c],d";
+
+        // Act
+        var actual = input.SafeSplit(',', '[', ']');
+
+        // Assert
+        actual.Should().HaveCount(3);
+        actual.ElementAt(0).Should().Be("a");
+        actual.ElementAt(1).Should().Be("b,c");
+        actual.ElementAt(2).Should().Be("d");
+    }
+
+    [Fact]
+    public void Can_SafeSplit_With_Empty_Last_Item()
+    {
+        // Arrange
+        var input = "a,\"b,c\",";
+
+        // Act
+        var actual = input.SafeSplit(',', '\"', '\"');
+
+        // Assert
+        actual.Should().HaveCount(3);
+        actual.ElementAt(0).Should().Be("a");
+        actual.ElementAt(1).Should().Be("b,c");
+        actual.ElementAt(2).Should().BeEmpty();
     }
 }
