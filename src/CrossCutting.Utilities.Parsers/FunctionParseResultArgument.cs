70882a3009f6c6a142de6c0cf462d3e2400c23ea
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-27 18:40:57 +0200

    Removed conversion from FunctionParseResult, you have to use IExpressionParser for this instead

--- src/CrossCutting.Utilities.Parsers/FunctionParseResultArgument.cs
+++ src/CrossCutting.Utilities.Parsers/FunctionParseResultArgument.cs
@@ -3,30 +3,6 @@
 public abstract record FunctionParseResultArgument
 {
     public abstract Result<object?> GetValue(IFunctionParseResultEvaluator evaluator);
-
-    public Result<int> GetValueInt32(string argumentName, IFormatProvider formatProvider, IFunctionParseResultEvaluator evaluator)
-    {
-        var result = GetValue(evaluator);
-        if (result.Value is string s)
-        {
-            return int.TryParse(s, NumberStyles.None, formatProvider, out var i)
-                ? Result<int>.Success(i)
-                : Result<int>.Invalid($"{argumentName} could not be parsed to an integer");
-        }
-        else if (result.Value is int i2)
-        {
-            return Result<int>.Success(i2);
-        }
-        else
-        {
-            return Result<int>.Invalid($"{argumentName} is not of type string or int");
-        }
-    }
-
-    public Result<string> GetValueString(string argumentName, IFormatProvider formatProvider, IFunctionParseResultEvaluator evaluator)
-        => GetValue(evaluator).Value is string s
-            ? Result<string>.Success(s)
-            : Result<string>.Invalid($"{argumentName} is not of type string");
 }
 
 public sealed record LiteralArgument : FunctionParseResultArgument
