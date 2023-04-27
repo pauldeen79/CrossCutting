289b35ee6287c999f9cfe72ab097ab42de6263fd
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-27 19:08:42 +0200

    This should be better. Need to add some tests and other types (long, boolean, DateTime).

--- src/CrossCutting.Utilities.Parsers/FunctionParseResult.cs
+++ src/CrossCutting.Utilities.Parsers/FunctionParseResult.cs
@@ -19,4 +19,46 @@ public record FunctionParseResult
         => index + 1 > Arguments.Count
             ? Result<object?>.Invalid($"Missing argument: {argumentName}")
             : Arguments[index].GetValue(evaluator);
+
+    public Result<string> GetArgumentStringValue(int index, string argumentName, IFunctionParseResultEvaluator evaluator)
+    {
+        var argumentValueResult = GetArgumentValue(index, argumentName, evaluator);
+        if (!argumentValueResult.IsSuccessful())
+        {
+            return Result<string>.FromExistingResult(argumentValueResult);
+        }
+
+        return argumentValueResult.Value is string stringValue
+            ? Result<string>.Success(stringValue)
+            : Result<string>.Invalid($"{argumentName} is not of type string");
+    }
+
+    public Result<int> GetArgumentInt32Value(int index, string argumentName, IFunctionParseResultEvaluator evaluator, IExpressionParser parser)
+    {
+        var argumentValueResult = GetArgumentValue(index, argumentName, evaluator);
+        if (argumentValueResult.Value is int i1)
+        {
+            return Result<int>.Success(i1);
+        }
+        else if (argumentValueResult.Value is string s)
+        {
+            var parseResult = parser.Parse(s, FormatProvider, Context);
+            if (!parseResult.IsSuccessful())
+            {
+                return Result<int>.Invalid($"{argumentName} is not of type integer");
+            }
+            else if (parseResult.Value is int i2)
+            {
+                return Result<int>.Success(i2);
+            }
+            else
+            {
+                return Result<int>.Invalid($"{argumentName} is not of type integer");
+            }
+        }
+        else
+        {
+            return Result<int>.Invalid($"{argumentName} is not of type integer");
+        }
+    }
 }
