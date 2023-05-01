d3898ccb15b313c5bf7687266e876709fd83b331
Paul Deen <paul.deen@dutchworkz.nl>
2023-05-01 20:27:59 +0200

    Added missing unit test

--- src/CrossCutting.Utilities.Parsers.Tests/NumericAggregatorTests.cs
+++ src/CrossCutting.Utilities.Parsers.Tests/NumericAggregatorTests.cs
@@ -18,4 +18,22 @@ public class NumericAggregatorTests
         // Assert
         result.Status.Should().Be(ResultStatus.Invalid);
     }
+
+    [Fact]
+    public void Evaluate_Returns_Error_When_Aggregator_Throws()
+    {
+        // Act
+        var result = NumericAggregator.Evaluate(1, 2, CultureInfo.InvariantCulture
+            , (_, _) => new object()
+            , (_, _) => new object()
+            , (_, _) => throw new InvalidOperationException("Kaboom")
+            , (_, _) => new object()
+            , (_, _) => new object()
+            , (_, _) => new object()
+            , (_, _) => new object());
+
+        // Assert
+        result.Status.Should().Be(ResultStatus.Error);
+        result.ErrorMessage.Should().Be("Aggregation failed. Error message: Kaboom");
+    }
 }
