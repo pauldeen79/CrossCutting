386ff7c9f4b13642eefaa801b716f7fe323a844f
Paul Deen <paul.deen@dutchworkz.nl>
2023-08-30 15:23:52 +0200

    Changed lifetime of parser components from Singleton to Scoped

--- src/CrossCutting.Utilities.Parsers/Extensions/ServiceCollectionExtensions.cs
+++ src/CrossCutting.Utilities.Parsers/Extensions/ServiceCollectionExtensions.cs
@@ -12,57 +12,57 @@ public static class ServiceCollectionExtensions
 
     private static IServiceCollection AddExpressionParser(this IServiceCollection services)
         => services
-        .AddSingleton<IExpressionParser, ExpressionParser>()
-        .AddSingleton<IExpressionParserProcessor, BooleanExpressionParserProcessor>()
-        .AddSingleton<IExpressionParserProcessor, ContextExpressionParserProcessor>()
-        .AddSingleton<IExpressionParserProcessor, NullExpressionParserProcessor>()
-        .AddSingleton<IExpressionParserProcessor, StringExpressionParserProcessor>()
-        .AddSingleton<IExpressionParserProcessor, NumericExpressionParserProcessor>()
-        .AddSingleton<IExpressionParserProcessor, DateTimeExpressionParserProcessor>();
+        .AddScoped<IExpressionParser, ExpressionParser>()
+        .AddScoped<IExpressionParserProcessor, BooleanExpressionParserProcessor>()
+        .AddScoped<IExpressionParserProcessor, ContextExpressionParserProcessor>()
+        .AddScoped<IExpressionParserProcessor, NullExpressionParserProcessor>()
+        .AddScoped<IExpressionParserProcessor, StringExpressionParserProcessor>()
+        .AddScoped<IExpressionParserProcessor, NumericExpressionParserProcessor>()
+        .AddScoped<IExpressionParserProcessor, DateTimeExpressionParserProcessor>();
 
     private static IServiceCollection AddExpressionStringParser(this IServiceCollection services)
         => services
-        .AddSingleton<IExpressionStringParser, ExpressionStringParser>()
-        .AddSingleton<IExpressionStringParserProcessor, EmptyExpressionProcessor>()
-        .AddSingleton<IExpressionStringParserProcessor, PipedExpressionProcessor>()
-        .AddSingleton<IExpressionStringParserProcessor, ConcatenateExpressionProcessor>()
-        .AddSingleton<IExpressionStringParserProcessor, LiteralExpressionProcessor>()
-        .AddSingleton<IExpressionStringParserProcessor, OnlyEqualsExpressionProcessor>()
-        .AddSingleton<IExpressionStringParserProcessor, FormattableStringExpressionProcessor>()
-        .AddSingleton<IExpressionStringParserProcessor, MathematicExpressionProcessor>()
-        .AddSingleton<IExpressionStringParserProcessor, EqualOperator>()
-        .AddSingleton<IExpressionStringParserProcessor, NotEqualOperator>()
-        .AddSingleton<IExpressionStringParserProcessor, GreaterThanOperator>()
-        .AddSingleton<IExpressionStringParserProcessor, GreaterOrEqualThanOperator>()
-        .AddSingleton<IExpressionStringParserProcessor, SmallerThanOperator>()
-        .AddSingleton<IExpressionStringParserProcessor, SmallerOrEqualThanOperator>();
+        .AddScoped<IExpressionStringParser, ExpressionStringParser>()
+        .AddScoped<IExpressionStringParserProcessor, EmptyExpressionProcessor>()
+        .AddScoped<IExpressionStringParserProcessor, PipedExpressionProcessor>()
+        .AddScoped<IExpressionStringParserProcessor, ConcatenateExpressionProcessor>()
+        .AddScoped<IExpressionStringParserProcessor, LiteralExpressionProcessor>()
+        .AddScoped<IExpressionStringParserProcessor, OnlyEqualsExpressionProcessor>()
+        .AddScoped<IExpressionStringParserProcessor, FormattableStringExpressionProcessor>()
+        .AddScoped<IExpressionStringParserProcessor, MathematicExpressionProcessor>()
+        .AddScoped<IExpressionStringParserProcessor, EqualOperator>()
+        .AddScoped<IExpressionStringParserProcessor, NotEqualOperator>()
+        .AddScoped<IExpressionStringParserProcessor, GreaterThanOperator>()
+        .AddScoped<IExpressionStringParserProcessor, GreaterOrEqualThanOperator>()
+        .AddScoped<IExpressionStringParserProcessor, SmallerThanOperator>()
+        .AddScoped<IExpressionStringParserProcessor, SmallerOrEqualThanOperator>();
 
     private static IServiceCollection AddFunctionParser(this IServiceCollection services)
         => services
-        .AddSingleton<IFunctionParser, FunctionParser>()
-        .AddSingleton<IFunctionParserNameProcessor, DefaultFunctionParserNameProcessor>()
-        .AddSingleton<IFunctionParserArgumentProcessor, FormattableStringFunctionParserArgumentProcessor>()
-        .AddSingleton<IFunctionParseResultEvaluator, DefaultFunctionParseResultEvaluator>();
+        .AddScoped<IFunctionParser, FunctionParser>()
+        .AddScoped<IFunctionParserNameProcessor, DefaultFunctionParserNameProcessor>()
+        .AddScoped<IFunctionParserArgumentProcessor, FormattableStringFunctionParserArgumentProcessor>()
+        .AddScoped<IFunctionParseResultEvaluator, DefaultFunctionParseResultEvaluator>();
 
     private static IServiceCollection AddFormattableStringParser(this IServiceCollection services)
         => services
-        .AddSingleton<IFormattableStringParser, FormattableStringParser>()
-        .AddSingleton<IFormattableStringStateProcessor, OpenSignProcessor>()
-        .AddSingleton<IFormattableStringStateProcessor, CloseSignProcessor>()
-        .AddSingleton<IFormattableStringStateProcessor, PlaceholderProcessor>()
-        .AddSingleton<IFormattableStringStateProcessor, ResultProcessor>()
-        .AddSingleton<IPlaceholderProcessor, UnknownPlaceholderProcessor>(); // used by CloseSignProcessor
+        .AddScoped<IFormattableStringParser, FormattableStringParser>()
+        .AddScoped<IFormattableStringStateProcessor, OpenSignProcessor>()
+        .AddScoped<IFormattableStringStateProcessor, CloseSignProcessor>()
+        .AddScoped<IFormattableStringStateProcessor, PlaceholderProcessor>()
+        .AddScoped<IFormattableStringStateProcessor, ResultProcessor>()
+        .AddScoped<IPlaceholderProcessor, UnknownPlaceholderProcessor>(); // used by CloseSignProcessor
 
     private static IServiceCollection AddMathematicExpressionParser(this IServiceCollection services)
         => services
-        .AddSingleton<IMathematicExpressionParser, MathematicExpressionParser>()
-        .AddSingleton<IMathematicExpressionProcessor, Validate>()
-        .AddSingleton<IMathematicExpressionProcessor, Recursion>()
-        .AddSingleton<IMathematicExpressionProcessor, MathematicOperators>()
-        .AddSingleton<IMathematicExpressionValidator, NullOrEmptyValidator>()
-        .AddSingleton<IMathematicExpressionValidator, TemporaryDelimiterValidator>()
-        .AddSingleton<IMathematicExpressionValidator, StartWithOperatorValidator>()
-        .AddSingleton<IMathematicExpressionValidator, EndWithOperatorValidator>()
-        .AddSingleton<IMathematicExpressionValidator, EmptyValuePartValidator>()
-        .AddSingleton<IMathematicExpressionValidator, BraceValidator>();
+        .AddScoped<IMathematicExpressionParser, MathematicExpressionParser>()
+        .AddScoped<IMathematicExpressionProcessor, Validate>()
+        .AddScoped<IMathematicExpressionProcessor, Recursion>()
+        .AddScoped<IMathematicExpressionProcessor, MathematicOperators>()
+        .AddScoped<IMathematicExpressionValidator, NullOrEmptyValidator>()
+        .AddScoped<IMathematicExpressionValidator, TemporaryDelimiterValidator>()
+        .AddScoped<IMathematicExpressionValidator, StartWithOperatorValidator>()
+        .AddScoped<IMathematicExpressionValidator, EndWithOperatorValidator>()
+        .AddScoped<IMathematicExpressionValidator, EmptyValuePartValidator>()
+        .AddScoped<IMathematicExpressionValidator, BraceValidator>();
 }
