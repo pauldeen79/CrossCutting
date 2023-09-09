46e22cd2ec592c9671c4bc517a0b1a4938266ccc
Paul Deen <paul.deen@dutchworkz.nl>
2023-09-09 19:58:13 +0200

    Minor: Added convencience overloads for Parse extension methods on ExpressionStringParser and FunctionParser to include the context, but exclude the IFormattableStringParser. Added missing unit tests for these extension methods.

--- src/CrossCutting.Utilities.Parsers/Extensions/FunctionParserExtensions.cs
+++ src/CrossCutting.Utilities.Parsers/Extensions/FunctionParserExtensions.cs
@@ -7,4 +7,7 @@ public static class FunctionParserExtensions
 
     public static Result<FunctionParseResult> Parse(this IFunctionParser instance, string input, IFormatProvider formatProvider, IFormattableStringParser formattableStringParser)
         => instance.Parse(input, formatProvider, null, formattableStringParser);
+
+    public static Result<FunctionParseResult> Parse(this IFunctionParser instance, string input, IFormatProvider formatProvider, object? context)
+        => instance.Parse(input, formatProvider, context, null);
 }
