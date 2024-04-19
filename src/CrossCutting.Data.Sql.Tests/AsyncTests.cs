f73fd7df8f52addc7424b0bb310aaa64c7270d92
Paul Deen <paul.deen@dutchworkz.nl>
2024-04-19 16:36:04 +0200

    Added test to check on async methods on SqlConnection. Now I only need to fit this into my structure...

---
+++ src/CrossCutting.Data.Sql.Tests/AsyncTests.cs
@@ -0,0 +1,16 @@
+﻿namespace CrossCutting.Data.Sql.Tests;
+
+public class AsyncTests
+{
+    [Fact(Skip = "Just a piece of code to check how to work with async stuff on Sql connections")]
+    public void Should_Be_Able_To_Do_Stuff_Async()
+    {
+        // Arrange
+        using var conn = new SqlConnection();
+        using var cmd = conn.CreateCommand();
+
+        cmd.ExecuteReaderAsync();
+        cmd.ExecuteNonQueryAsync();
+        cmd.ExecuteScalarAsync();
+    }
+}
