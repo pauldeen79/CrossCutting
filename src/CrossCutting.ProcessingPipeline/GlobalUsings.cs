f65425013f1cd0d9f4cf1793ad39436ea8d7581b
Paul Deen <paul.deen@dutchworkz.nl>
2024-11-06 19:07:55 +0100

    Refactor: Moved IBuilder to CrossCutting.Common.Abstractions

--- src/CrossCutting.ProcessingPipeline/GlobalUsings.cs
+++ src/CrossCutting.ProcessingPipeline/GlobalUsings.cs
@@ -6,6 +6,7 @@ global using System.Linq;
 global using System.Threading;
 global using System.Threading.Tasks;
 global using CrossCutting.Common;
+global using CrossCutting.Common.Abstractions;
 global using CrossCutting.Common.DataAnnotations;
 global using CrossCutting.Common.Extensions;
 global using CrossCutting.Common.Results;
