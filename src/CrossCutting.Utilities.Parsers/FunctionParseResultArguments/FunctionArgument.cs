8ff5858b4a6d3b20a774ed6dd6f5d1cfc85b755e
Paul Deen <paul.deen@dutchworkz.nl>
2024-12-10 16:21:29 +0100

    Minor: Separated argument null check and operation on argument

--- src/CrossCutting.Utilities.Parsers/FunctionParseResultArguments/FunctionArgument.cs
+++ src/CrossCutting.Utilities.Parsers/FunctionParseResultArguments/FunctionArgument.cs
@@ -3,5 +3,9 @@
 public partial record FunctionArgument
 {
     public override Result<object?> GetValueResult(object? context, IFunctionParseResultEvaluator evaluator, IExpressionParser parser, IFormatProvider formatProvider)
-        => ArgumentGuard.IsNotNull(evaluator, nameof(evaluator)).Evaluate(Function, parser, context);
+    {
+        evaluator = ArgumentGuard.IsNotNull(evaluator, nameof(evaluator));
+
+        return evaluator.Evaluate(Function, parser, context);
+    }
 }
