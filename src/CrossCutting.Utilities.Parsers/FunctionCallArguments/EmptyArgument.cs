956b10e395d5ce38777dacc04453fade97844e20
Paul Deen <paul.deen@dutchworkz.nl>
2024-12-29 13:27:01 +0100

    Removed unnecessary null checks

--- src/CrossCutting.Utilities.Parsers/FunctionCallArguments/EmptyArgument.cs
+++ src/CrossCutting.Utilities.Parsers/FunctionCallArguments/EmptyArgument.cs
@@ -3,14 +3,8 @@
 public partial record EmptyArgument
 {
     public override Result<object?> GetValueResult(object? context, IFunctionEvaluator evaluator, IExpressionParser parser, IFormatProvider formatProvider)
-    {
-        parser = ArgumentGuard.IsNotNull(parser, nameof(parser));
-
-        return Result.Success(default(object?));
-    }
+        => Result.Success(default(object?));
 
     public override Result ValidateValueResult(object? context, IFunctionEvaluator evaluator, IExpressionParser parser, IFormatProvider formatProvider)
-    {
-        return Result.Success();
-    }
+        => Result.Success();
 }
