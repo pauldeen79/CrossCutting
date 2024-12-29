b4fb573a2da7ea9a0ea1311e387c27bc71f63810
Paul Deen <paul.deen@dutchworkz.nl>
2024-12-29 16:30:13 +0100

    Minor: Remove and sort usings

--- src/CrossCutting.Utilities.Parsers/GlobalUsings.cs
+++ src/CrossCutting.Utilities.Parsers/GlobalUsings.cs
@@ -1,6 +1,5 @@
 ﻿global using System;
 global using System.Collections.Generic;
-global using System.Collections.ObjectModel;
 global using System.ComponentModel.DataAnnotations;
 global using System.Globalization;
 global using System.IO;
