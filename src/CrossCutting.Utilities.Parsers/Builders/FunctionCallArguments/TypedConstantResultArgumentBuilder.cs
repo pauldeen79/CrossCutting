144a92322d74b26ad78ddbe09c63833f28aed6a5
Paul Deen <paul.deen@dutchworkz.nl>
2025-01-17 20:59:50 +0100

    Added TypedConstantResultArgument and TypedConstantResultArgumentBuilder

---
+++ src/CrossCutting.Utilities.Parsers/Builders/FunctionCallArguments/TypedConstantResultArgumentBuilder.cs
@@ -0,0 +1,21 @@
+﻿namespace CrossCutting.Utilities.Parsers.Builders.FunctionCallArguments;
+
+public partial class TypedConstantResultArgumentBuilder<T> : TypedFunctionCallArgumentBuilder<T>
+{
+    public FunctionCallArgumentBuilder ToUntyped()
+        => new ConstantResultArgumentBuilder().WithResult(Result.Transform<object?>(value => value));
+
+    public override TypedFunctionCallArgument<T> BuildTyped()
+        => new TypedConstantResultArgument<T>(Result);
+
+    public override FunctionCallArgument Build()
+        => new TypedConstantResultArgument<T>(Result);
+
+    public Result<T> Result { get; set; } = default!;
+
+    public TypedConstantResultArgumentBuilder<T> WithResult(Result<T> result)
+    {
+        Result = result;
+        return this;
+    }
+}
---
+++ src/CrossCutting.Utilities.Parsers/FunctionCallArguments/TypedConstantResultArgument.cs
@@ -0,0 +1,23 @@
+﻿namespace CrossCutting.Utilities.Parsers.FunctionCallArguments;
+
+public partial record TypedConstantResultArgument<T> : TypedFunctionCallArgument<T>
+{
+    public Result<T> GetTypedValueResult(FunctionCallContext context)
+        => Result;
+
+    public override Result<object?> GetValueResult(FunctionCallContext context)
+        => Result.Transform<object?>(value => value);
+
+    public override FunctionCallArgumentBuilder ToBuilder()
+        => new TypedConstantResultArgumentBuilder<T>().WithResult(Result);
+
+    public override TypedFunctionCallArgumentBuilder<T> ToTypedBuilder()
+        => new TypedConstantResultArgumentBuilder<T>().WithResult(Result);
+
+    public Result<T> Result { get; }
+
+    public TypedConstantResultArgument(Result<T> result)
+    {
+        Result = result;
+    }
+}
