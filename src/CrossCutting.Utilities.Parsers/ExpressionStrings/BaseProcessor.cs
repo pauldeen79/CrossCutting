767fde85bca90f7f83047d545527175db76ee7a2
Paul Deen <paul.deen@dutchworkz.nl>
2025-01-29 20:46:06 +0100

    Removed unused code

--- src/CrossCutting.Utilities.Parsers/ExpressionStrings/BaseProcessor.cs
+++ src/CrossCutting.Utilities.Parsers/ExpressionStrings/BaseProcessor.cs
@@ -18,22 +18,6 @@ internal static class BaseProcessor
         return validDelegate(split);
     }
 
-    internal static Result SplitDelimited(ExpressionStringEvaluatorContext context, char splitDelimiter, Func<string[], Result> validDelegate)
-    {
-        if (context.Input.IndexOf(splitDelimiter) == -1)
-        {
-            return Result.Continue();
-        }
-
-        var split = context.Input.Substring(1).SplitDelimited(splitDelimiter, '\"', true, true);
-        if (split.Length == 1)
-        {
-            return Result.Continue();
-        }
-
-        return validDelegate(split);
-    }
-
     internal static Func<string[], Result<Type>> Parse(ExpressionStringEvaluatorContext context)
         => split => Result.Aggregate(split.Select(item => context.Parser.Validate($"={item}", context.Settings, context.Context, context.FormattableStringParser)), Result.NoContent<Type>(), validationResults => Result.Invalid<Type>("Validation failed, see inner results for details", validationResults));
 }
