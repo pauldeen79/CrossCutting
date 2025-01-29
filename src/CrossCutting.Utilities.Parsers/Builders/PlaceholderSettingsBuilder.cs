235ce77e2767629cf00c091e3157aee0067d1eab
Paul Deen <paul.deen@dutchworkz.nl>
2025-01-29 20:48:37 +0100

    Removed more unused code

--- src/CrossCutting.Utilities.Parsers/Builders/PlaceholderSettingsBuilder.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CrossCutting.Utilities.Parsers.Builders;
-
-public partial class PlaceholderSettingsBuilder
-{
-    partial void SetDefaultValues()
-    {
-        FormatProvider = CultureInfo.InvariantCulture;
-    }
-}
