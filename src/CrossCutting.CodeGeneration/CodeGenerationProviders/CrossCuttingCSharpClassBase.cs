b91b3898aa05fbd09347654d10804875ddea8fe4
Paul Deen <paul.deen@dutchworkz.nl>
2025-02-02 20:35:28 +0100

    Added missing namespace

--- src/CrossCutting.CodeGeneration/CodeGenerationProviders/CrossCuttingCSharpClassBase.cs
+++ src/CrossCutting.CodeGeneration/CodeGenerationProviders/CrossCuttingCSharpClassBase.cs
@@ -19,7 +19,7 @@ public abstract class CrossCuttingCSharpClassBase(IPipelineService pipelineServi
     protected override bool EnableGlobalUsings => true;
     protected override bool AddImplicitOperatorOnBuilder => true;
     protected override bool UseBuilderAbstractionsTypeConversion => true;
-    protected override string[] GetBuilderAbstractionsTypeConversionNamespaces() => [ $"{CoreNamespace}.Abstractions" ];
+    protected override string[] GetBuilderAbstractionsTypeConversionNamespaces() => [ $"{CoreNamespace}.Abstractions", $"{CoreNamespace}.Builders.Abstractions"];
 
     protected override IEnumerable<TypenameMappingBuilder> CreateAdditionalTypenameMappings()
     {
