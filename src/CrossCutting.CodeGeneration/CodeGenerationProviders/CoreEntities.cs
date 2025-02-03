1a5fabb4aef45858f6b49b4dfd518b2431608bba
Paul Deen <paul.deen@dutchworkz.nl>
2025-02-03 19:34:44 +0100

    We don't need these quirks. In fact, they caused problems.

--- src/CrossCutting.CodeGeneration/CodeGenerationProviders/CoreEntities.cs
+++ src/CrossCutting.CodeGeneration/CodeGenerationProviders/CoreEntities.cs
@@ -3,8 +3,6 @@
 [ExcludeFromCodeCoverage]
 public class CoreEntities(IPipelineService pipelineService) : CrossCuttingCSharpClassBase(pipelineService)
 {
-    protected override bool UseBuilderAbstractionsTypeConversion => false; //quirk
-
     public override string Path => Constants.Namespaces.UtilitiesParsers;
 
     public override Task<Result<IEnumerable<TypeBase>>> GetModel(CancellationToken cancellationToken)
