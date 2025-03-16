4a1242f2a9a2dceaf3322772cc5cdfc547f2e694
Paul Deen <paul.deen@dutchworkz.nl>
2025-03-16 14:02:12 +0100

    Implemented Validate method

--- src/CrossCutting.Utilities.Parsers/Expressions/OperatorExpression.cs
+++ src/CrossCutting.Utilities.Parsers/Expressions/OperatorExpression.cs
@@ -17,7 +17,7 @@ public class OperatorExpression : IExpression
             return Result.Continue<object?>();
         }
 
-        string[] parts = Regex.Split(context.Expression, matches[0].Value);
+        var parts = Regex.Split(context.Expression, matches[0].Value);
         if (parts.Length != 2)
         {
             // More than one operator
@@ -41,10 +41,46 @@ public class OperatorExpression : IExpression
         return operatorSymbol switch
         {
             "==" => Equal.Evaluate(leftOperandResult.Value, rightOperandResult.Value, context.Settings.StringComparison).Transform<object?>(x => x),
-            _ => GreaterThan.Evaluate(leftOperandResult.Value, rightOperandResult.Value).Transform<object?>(x => x)
+            "!=" => NotEqual.Evaluate(leftOperandResult.Value, rightOperandResult.Value, context.Settings.StringComparison).Transform<object?>(x => x),
+            "<" => SmallerThan.Evaluate(leftOperandResult.Value, rightOperandResult.Value).Transform<object?>(x => x),
+            "<=" => SmallerOrEqualThan.Evaluate(leftOperandResult.Value, rightOperandResult.Value).Transform<object?>(x => x),
+            ">" => GreaterThan.Evaluate(leftOperandResult.Value, rightOperandResult.Value).Transform<object?>(x => x),
+            //">="
+            _ => GreaterOrEqualThan.Evaluate(leftOperandResult.Value, rightOperandResult.Value).Transform<object?>(x => x)
         };
     }
 
     public Result<Type> Validate(ExpressionEvaluatorContext context)
-        => Result.Continue<Type>(); //TODO: Implement this
+    {
+        context = ArgumentGuard.IsNotNull(context, nameof(context));
+
+        var matches = _operatorRegEx.Matches(context.Expression);
+
+        // Ensure there's exactly one operator
+        if (matches.Count != 1)
+        {
+            return Result.Continue<Type>();
+        }
+
+        var parts = Regex.Split(context.Expression, matches[0].Value);
+        if (parts.Length != 2)
+        {
+            // More than one operator
+            return Result.Continue<Type>();
+        }
+
+        var leftOperandResult = context.Evaluator.Validate(parts[0].Trim(), new ExpressionEvaluatorSettingsBuilder().WithFormatProvider(context.Settings.FormatProvider), context.Context);
+        if (!leftOperandResult.IsSuccessful())
+        {
+            return Result.Invalid<Type>("Left operand is invalid, see inner results for more details", [leftOperandResult]);
+        }
+
+        var rightOperandResult = context.Evaluator.Validate(parts[1].Trim(), new ExpressionEvaluatorSettingsBuilder().WithFormatProvider(context.Settings.FormatProvider), context.Context);
+        if (!rightOperandResult.IsSuccessful())
+        {
+            return Result.Invalid<Type>("Right operand is invalid, see inner results for more details", [rightOperandResult]);
+        }
+
+        return Result.Success(typeof(bool));
+    }
 }
