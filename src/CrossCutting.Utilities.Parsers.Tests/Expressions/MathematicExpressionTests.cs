3c14be465aac6f823fe58ab70486456b832ec692
Paul Deen <paul.deen@dutchworkz.nl>
2025-03-21 16:08:44 +0100

    Added missing unit tests

---
+++ src/CrossCutting.Utilities.Parsers.Tests/Expressions/MathematicExpressionTests.cs
@@ -0,0 +1,54 @@
+﻿namespace CrossCutting.Utilities.Parsers.Tests.Expressions;
+
+public class MathematicExpressionTests
+{
+    protected IMathematicExpression Expression { get; }
+    protected IExpressionEvaluator Evaluator { get; }
+
+    protected MathematicExpression CreateSut() => new MathematicExpression([Expression]);
+
+    protected ExpressionEvaluatorContext CreateContext(string expression)
+        => new ExpressionEvaluatorContext(expression, new ExpressionEvaluatorSettingsBuilder(), null, Evaluator);
+
+    protected MathematicExpressionTests()
+    {
+        Expression = Substitute.For<IMathematicExpression>();
+        Evaluator = Substitute.For<IExpressionEvaluator>();
+    }
+
+    public class Evaluate : MathematicExpressionTests
+    {
+        [Fact]
+        public void Returns_Non_Successful_Result_From_Mathematic_Expression()
+        {
+            // Arrange
+            Expression.Evaluate(Arg.Any<MathematicExpressionState>()).Returns(Result.Error<MathematicExpressionState>("Kaboom"));
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.Evaluate(CreateContext("some expression"));
+
+            // Assert
+            result.Status.ShouldBe(ResultStatus.Error);
+            result.ErrorMessage.ShouldBe("Kaboom");
+        }
+    }
+
+    public class Validate : MathematicExpressionTests
+    {
+        [Fact]
+        public void Returns_Non_Successful_Result_From_Mathematic_Expression()
+        {
+            // Arrange
+            Expression.Evaluate(Arg.Any<MathematicExpressionState>()).Returns(Result.Error<MathematicExpressionState>("Kaboom"));
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.Validate(CreateContext("some expression"));
+
+            // Assert
+            result.Status.ShouldBe(ResultStatus.Error);
+            result.ErrorMessage.ShouldBe("Kaboom");
+        }
+    }
+}
---
+++ src/CrossCutting.Utilities.Parsers.Tests/MathematicExpressions/Aggregators/BinaryOrAggregatorTests.cs
@@ -0,0 +1,32 @@
+﻿namespace CrossCutting.Utilities.Parsers.Tests.MathematicExpressions.Aggregators;
+
+public class BinaryOrAggregatorTests
+{
+    [Fact]
+    public void Aggregate_Returns_Invalid_When_LeftExpression_Is_Not_Boolean()
+    {
+        // Arrange
+        var sut = new BinaryOrAggregator();
+
+        // Act
+        var result = sut.Aggregate("not a boolean", true, CultureInfo.InvariantCulture);
+
+        // Assert
+        result.Status.ShouldBe(ResultStatus.Invalid);
+        result.ErrorMessage.ShouldBe("Binary or (|) operator can only be used on boolean values");
+    }
+
+    [Fact]
+    public void Aggregate_Returns_Invalid_When_RightExpression_Is_Not_Boolean()
+    {
+        // Arrange
+        var sut = new BinaryOrAggregator();
+
+        // Act
+        var result = sut.Aggregate(false, "not a boolean", CultureInfo.InvariantCulture);
+
+        // Assert
+        result.Status.ShouldBe(ResultStatus.Invalid);
+        result.ErrorMessage.ShouldBe("Binary or (|) operator can only be used on boolean values");
+    }
+}
