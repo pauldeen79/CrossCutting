b7949e8997448aff73987933bdf733e1a96b4c03
Paul Deen <paul.deen@dutchworkz.nl>
2025-03-26 19:36:16 +0100

    Added integration tests for each type of expression currently implemented

---
+++ src/CrossCutting.Utilities.ExpressionEvaluator.Tests/IntegrationTests.cs
@@ -0,0 +1,51 @@
+﻿namespace CrossCutting.Utilities.ExpressionEvaluator.Tests;
+
+public sealed class IntegrationTests : TestBase, IDisposable
+{
+    [Fact]
+    public void Can_Evaluate_Binary_Operator_Expression()
+    {
+        // Arrange
+        var sut = CreateSut();
+        var expression = "true && true && \"string value\"";
+
+        // Act
+        var result = sut.Evaluate(CreateContext(expression));
+
+        // Assert
+        result.Status.ShouldBe(ResultStatus.Ok);
+        result.Value.ShouldBe(true);
+    }
+
+    [Fact]
+    public void Can_Evaluate_Comparison_Operator_Expression()
+    {
+        // Arrange
+        var sut = CreateSut();
+        var expression = "2 > 1";
+
+        // Act
+        var result = sut.Evaluate(CreateContext(expression));
+
+        // Assert
+        result.Status.ShouldBe(ResultStatus.Ok);
+        result.Value.ShouldBe(true);
+    }
+
+    public IntegrationTests()
+    {
+        Provider = new ServiceCollection().AddExpressionEvaluator().BuildServiceProvider();
+        Scope = Provider.CreateScope();
+    }
+
+    private ServiceProvider Provider { get; set; }
+    private IServiceScope Scope { get; set; }
+
+    private IExpressionEvaluator CreateSut() => Scope.ServiceProvider.GetRequiredService<IExpressionEvaluator>();
+
+    public void Dispose()
+    {
+        Scope.Dispose();
+        Provider.Dispose();
+    }
+}
