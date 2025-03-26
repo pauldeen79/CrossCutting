793374e6949d7485c30f5daa3598cd09b566d3b2
Paul Deen <paul.deen@dutchworkz.nl>
2025-03-26 15:17:22 +0100

    Minor: Fixed namespaces in CrossCutting.CodeGeneration

--- src/CrossCutting.CodeGeneration/CodeGenerationProviders/AbstractBuilders.cs
+++ src/CrossCutting.CodeGeneration/CodeGenerationProviders/AbstractBuilders.cs
@@ -1,4 +1,4 @@
-﻿namespace CrossCutting.CodeGeneration.CodeGenerationProviders.FunctionParseResultArguments;
+﻿namespace CrossCutting.CodeGeneration.CodeGenerationProviders;
 
 [ExcludeFromCodeCoverage]
 public class AbstractBuilders(IPipelineService pipelineService) : CrossCuttingCSharpClassBase(pipelineService)
