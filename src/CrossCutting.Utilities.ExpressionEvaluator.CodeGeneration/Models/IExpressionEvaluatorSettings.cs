260d71b2ac79a9fed6bb39aca01c55b81263a3f0
Paul Deen <paul.deen@dutchworkz.nl>
2025-03-29 08:23:23 +0100

    Minor: Removed code comment

--- src/CrossCutting.Utilities.ExpressionEvaluator.CodeGeneration/Models/IExpressionEvaluatorSettings.cs
+++ src/CrossCutting.Utilities.ExpressionEvaluator.CodeGeneration/Models/IExpressionEvaluatorSettings.cs
@@ -4,6 +4,5 @@ internal interface IExpressionEvaluatorSettings
 {
     IFormatProvider FormatProvider { get; }
     StringComparison StringComparison { get; }
-    //[DefaultValue(true)] bool ValidateArgumentTypes { get; }
     [DefaultValue(10)] int MaximumRecursion { get; }
 }
