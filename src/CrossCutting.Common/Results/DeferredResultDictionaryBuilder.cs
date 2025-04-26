d9e8be85d32587c371c34bcad726729127b2a201
Paul Deen <paul.deen@dutchworkz.nl>
2025-04-26 15:59:01 +0200

    Added DeferredResultDictionaryBuilder, as a helper class to fill context (lazy variables) for ExpressionEvaluator

---
+++ src/CrossCutting.Common/Results/DeferredResultDictionaryBuilder.cs
@@ -0,0 +1,33 @@
+﻿namespace CrossCutting.Common.Results;
+
+public class DeferredResultDictionaryBuilder
+{
+    private readonly Dictionary<string, Func<Result>> _resultset = new();
+
+    public DeferredResultDictionaryBuilder Add(string name, Func<Result> value)
+    {
+        ArgumentGuard.IsNotNull(value, nameof(value));
+
+        _resultset.Add(name, value);
+        return this;
+    }
+
+    public IReadOnlyDictionary<string, Func<Result>> Build()
+        => _resultset;
+}
+
+public class DeferredResultDictionaryBuilder<T>
+{
+    private readonly Dictionary<string, Func<Result<T>>> _resultset = new();
+
+    public DeferredResultDictionaryBuilder<T> Add(string name, Func<Result<T>> value)
+    {
+        ArgumentGuard.IsNotNull(value, nameof(value));
+
+        _resultset.Add(name, value);
+        return this;
+    }
+
+    public IReadOnlyDictionary<string, Func<Result<T>>> Build()
+        => _resultset;
+}
