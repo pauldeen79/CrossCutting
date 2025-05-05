5389a45cae1046c6a85c0a255f9f0e7bb1206dbf
Paul Deen <paul.deen@dutchworkz.nl>
2025-05-05 11:25:26 +0200

    Fixed Sonar issue

--- src/CrossCutting.Common.Tests/Results/ResultTests.cs
+++ src/CrossCutting.Common.Tests/Results/ResultTests.cs
@@ -3504,12 +3504,12 @@ public class ResultTests
 
     public sealed record MyResult: Result
     {
-        public MyResult(object value) : base(ResultStatus.Ok, null, Enumerable.Empty<ValidationError>(), Enumerable.Empty<Result>(), null)
+        public MyResult(object? value) : base(ResultStatus.Ok, null, Enumerable.Empty<ValidationError>(), Enumerable.Empty<Result>(), null)
         {
             Value = value;
         }
 
-        public object Value { get; }
+        public object? Value { get; }
 
         public override object? GetValue() => Value;
     }
