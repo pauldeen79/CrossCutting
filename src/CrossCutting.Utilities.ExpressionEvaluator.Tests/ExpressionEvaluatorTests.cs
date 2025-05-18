2ebc0600a1e2c97097d43aa91c44b9311499fd0f
Paul Deen <paul.deen@dutchworkz.nl>
2025-05-18 15:40:48 +0200

    Added missing unit test

--- src/CrossCutting.Utilities.ExpressionEvaluator.Tests/ExpressionEvaluatorTests.cs
+++ src/CrossCutting.Utilities.ExpressionEvaluator.Tests/ExpressionEvaluatorTests.cs
@@ -162,6 +162,87 @@ public class ExpressionEvaluatorTests : TestBase<ExpressionEvaluator>
         }
     }
 
+    public class EvaluateTypedCallbackAsync : ExpressionEvaluatorTests
+    {
+        [Fact]
+        public async Task Returns_Invalid_When_Expression_Is_Null_Or_Empty()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = await sut.EvaluateTypedCallbackAsync<string>(CreateContext(string.Empty), CancellationToken.None);
+
+            // Assert
+            result.Status.ShouldBe(ResultStatus.Invalid);
+            result.ErrorMessage.ShouldBe("Value is required");
+        }
+
+        [Fact]
+        public async Task Returns_First_Understood_Result_When_Not_Equal_To_Continue()
+        {
+            // Arrange
+            Expression.ParseAsync(Arg.Any<ExpressionEvaluatorContext>(), Arg.Any<CancellationToken>()).Returns(new ExpressionParseResultBuilder().WithStatus(ResultStatus.Continue));
+            Expression.EvaluateAsync(Arg.Any<ExpressionEvaluatorContext>(), Arg.Any<CancellationToken>()).Returns(Result.Success<object?>("result value"));
+            var sut = CreateSut();
+
+            // Act
+            var result = await sut.EvaluateTypedCallbackAsync<string>(CreateContext("expression"), CancellationToken.None);
+
+            // Assert
+            result.Status.ShouldBe(ResultStatus.Ok);
+            result.Value.ShouldBe("result value");
+        }
+
+        [Fact]
+        public async Task Returns_First_Understood_TypedResult_When_Not_Successful()
+        {
+            // Arrange
+            var typedExpression = Substitute.For<IExpressionComponent<string>>();
+            typedExpression.ParseAsync(Arg.Any<ExpressionEvaluatorContext>(), Arg.Any<CancellationToken>()).Returns(new ExpressionParseResultBuilder().WithStatus(ResultStatus.Continue));
+            typedExpression.EvaluateTypedAsync(Arg.Any<ExpressionEvaluatorContext>(), Arg.Any<CancellationToken>()).Returns(Result.Error<string>("Kaboom"));
+            var sut = new ExpressionEvaluator(new ExpressionTokenizer(), new ExpressionParser(), [typedExpression]);
+
+            // Act
+            var result = await sut.EvaluateTypedCallbackAsync<string>(CreateContext("expression"), CancellationToken.None);
+
+            // Assert
+            result.Status.ShouldBe(ResultStatus.Error);
+            result.ErrorMessage.ShouldBe("Kaboom");
+        }
+
+        [Fact]
+        public async Task Returns_Invalid_When_Expression_Is_Not_Understood()
+        {
+            // Arrange
+            Expression.ParseAsync(Arg.Any<ExpressionEvaluatorContext>(), Arg.Any<CancellationToken>()).Returns(new ExpressionParseResultBuilder().WithStatus(ResultStatus.Continue));
+            Expression.EvaluateAsync(Arg.Any<ExpressionEvaluatorContext>(), Arg.Any<CancellationToken>()).Returns(Result.Continue<object?>());
+            var sut = CreateSut();
+
+            // Act
+            var result = await sut.EvaluateTypedCallbackAsync<string>(CreateContext("expression"), CancellationToken.None);
+
+            // Assert
+            result.Status.ShouldBe(ResultStatus.Invalid);
+            result.ErrorMessage.ShouldBe("Unknown expression type found in fragment: expression");
+        }
+
+        [Fact]
+        public async Task Returns_Invalid_When_Maximum_Recursion_Has_Been_Reached()
+        {
+            // Arrange
+            Expression.EvaluateAsync(Arg.Any<ExpressionEvaluatorContext>(), Arg.Any<CancellationToken>()).Returns(Result.Success<object?>("result value"));
+            var sut = CreateSut();
+
+            // Act
+            var result = await sut.EvaluateTypedCallbackAsync<string>(new ExpressionEvaluatorContext("expression", new ExpressionEvaluatorSettingsBuilder(), sut, null, int.MaxValue), CancellationToken.None);
+
+            // Assert
+            result.Status.ShouldBe(ResultStatus.Invalid);
+            result.ErrorMessage.ShouldBe("Maximum recursion level has been reached");
+        }
+    }
+
     public class ParseAsync : ExpressionEvaluatorTests
     {
         [Fact]
