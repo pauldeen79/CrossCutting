4446f493be1306815ac74d9c6edcc6fb9ce6a622
Paul Deen <paul.deen@dutchworkz.nl>
2025-06-07 17:34:51 +0200

    Added missing test for changed code

---
+++ src/CrossCutting.Utilities.ExpressionEvaluator.Tests/DotExpressionComponents/ReflectionMethodDotExpressionComponentTests.cs
@@ -0,0 +1,27 @@
+﻿namespace CrossCutting.Utilities.ExpressionEvaluator.Tests.DotExpressionComponents;
+
+public class ReflectionMethodDotExpressionComponentTests : TestBase<ReflectionMethodDotExpressionComponent>
+{
+    protected IFunctionParser FunctionParser => Mocks.GetOrCreate<IFunctionParser>(ClassFactory);
+
+    public class EvaluateAsync : ReflectionMethodDotExpressionComponentTests
+    {
+        [Fact]
+        public async Task Returns_Unsuccessful_Result_From_FunctionParseResult()
+        {
+            // Arrange
+            FunctionParser
+                .Parse(Arg.Any<ExpressionEvaluatorContext>())
+                .Returns(Result.Error<FunctionCall>("Kaboom"));
+            var sut = CreateSut();
+            var context = CreateContext("Dummy", settings: new ExpressionEvaluatorSettingsBuilder().WithAllowReflection());
+
+            // Act
+            var result = await sut.EvaluateAsync(new DotExpressionComponentState(context, FunctionParser, Result.NoContent<object?>("Kaboom"), "Dummy"), CancellationToken.None);
+
+            // Assert
+            result.Status.ShouldBe(ResultStatus.Error);
+            result.ErrorMessage.ShouldBe("Kaboom");
+        }
+    }
+}
