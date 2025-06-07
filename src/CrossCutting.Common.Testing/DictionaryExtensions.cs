57d84381b3073d92c1c38fa2857b65313d14fb99
Paul Deen <paul.deen@dutchworkz.nl>
2025-06-07 17:11:19 +0200

    Added convenience method on dictionary with mock objects, to get or create a mock

---
+++ src/CrossCutting.Common.Testing/DictionaryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrossCutting.Common.Testing;
+
+public static class DictionaryExtensions
+{
+    public static T GetOrCreate<T>(this IDictionary<Type, object?> instance, Func<Type, object?> classFactory)
+    {
+        if (instance.ContainsKey(typeof(T)))
+        {
+            return (T)instance[typeof(T)]!;
+        }
+
+        var newInstance = Testing.CreateInstance<T>(classFactory, instance)
+            ?? throw new InvalidOperationException($"Class factory did not create an instance of type {typeof(T).FullName}");
+
+        instance[typeof(T)] = newInstance;
+
+        return newInstance;
+    }
+}
