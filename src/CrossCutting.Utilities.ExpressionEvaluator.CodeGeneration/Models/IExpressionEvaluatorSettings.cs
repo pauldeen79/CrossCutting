4de901d54bdf01a679f999bc90c2c8186761fb1f
Paul Deen <paul.deen@dutchworkz.nl>
2025-06-15 15:30:22 +0200

    Changed maximum recursion of ExpressionEvaluator from 10 to 50, and fixed generating ExpressionEvaluator models

--- src/CrossCutting.Utilities.ExpressionEvaluator.CodeGeneration/Models/IExpressionEvaluatorSettings.cs
+++ src/CrossCutting.Utilities.ExpressionEvaluator.CodeGeneration/Models/IExpressionEvaluatorSettings.cs
@@ -4,11 +4,11 @@ internal interface IExpressionEvaluatorSettings
 {
     IFormatProvider FormatProvider { get; }
     StringComparison StringComparison { get; }
-    [DefaultValue(10)] int MaximumRecursion { get; }
+    [DefaultValue(50)] int MaximumRecursion { get; }
     [DefaultValue(true)] bool EscapeBraces { get; }
     [DefaultValue(true)] bool ValidateArgumentTypes { get; }
     [DefaultValue(false)] bool StrictTypeChecking { get; }
     [DefaultValue(false)] bool AllowReflection { get; }
-    [Required] [DefaultValue("{")] string PlaceholderStart { get; }
-    [Required] [DefaultValue("}")] string PlaceholderEnd { get; }
+    [Required] /*[DefaultValue("{")]*/ string PlaceholderStart { get; }
+    [Required] /*[DefaultValue("}")]*/ string PlaceholderEnd { get; }
 }
---
+++ src/CrossCutting.Utilities.ExpressionEvaluator/Builders/ExpressionEvaluatorSettingsBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace CrossCutting.Utilities.ExpressionEvaluator.Builders;
+
+public partial class ExpressionEvaluatorSettingsBuilder
+{
+    partial void SetDefaultValues()
+    {
+        _placeholderStart = "{";
+        _placeholderEnd = "}";
+    }
+}
