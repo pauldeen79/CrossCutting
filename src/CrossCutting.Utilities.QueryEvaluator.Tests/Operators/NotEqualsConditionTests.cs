a62c19170c58adfe5976be4e0a2ff37541fe836f
Paul Deen <paul.deen@dutchworkz.nl>
2025-07-01 20:03:46 +0200

    Added missing tests

---
+++ src/CrossCutting.Utilities.QueryEvaluator.Tests/Operators/NotEqualsConditionTests.cs
@@ -0,0 +1,53 @@
+﻿namespace CrossCutting.Utilities.QueryEvaluator.Tests.Operators;
+
+public class NotEqualsConditionTests : TestBase<NotEqualsCondition>
+{
+    public class Evaluate : NotEqualsConditionTests
+    {
+        [Fact]
+        public async Task Returns_Ok_On_Two_Strings()
+        {
+            // Arrange
+            var leftValue = "this";
+            var rightValue = "this";
+            var parameters = new Dictionary<string, object?>
+            {
+                { nameof(IDoubleExpressionContainer.FirstExpression).ToCamelCase(CultureInfo.CurrentCulture), new LiteralExpressionBuilder().WithValue(leftValue).Build() },
+                { nameof(IDoubleExpressionContainer.SecondExpression).ToCamelCase(CultureInfo.CurrentCulture), new LiteralExpressionBuilder().WithValue(rightValue).Build() },
+            };
+            var sut = CreateSut(parameters);
+            var context = CreateContext("Dummy");
+
+
+            // Act
+            var result = await sut.EvaluateTypedAsync(context, CancellationToken.None);
+
+            // Assert
+            result.Status.ShouldBe(ResultStatus.Ok);
+            result.Value.ShouldBe(false);
+        }
+
+        [Fact]
+        public async Task Returns_Ok_On_Different_Types()
+        {
+            // Arrange
+            var leftValue = "this";
+            var rightValue = 13;
+            var parameters = new Dictionary<string, object?>
+            {
+                { nameof(IDoubleExpressionContainer.FirstExpression).ToCamelCase(CultureInfo.CurrentCulture), new LiteralExpressionBuilder().WithValue(leftValue).Build() },
+                { nameof(IDoubleExpressionContainer.SecondExpression).ToCamelCase(CultureInfo.CurrentCulture), new LiteralExpressionBuilder().WithValue(rightValue).Build() },
+            };
+            var sut = CreateSut(parameters);
+            var context = CreateContext("Dummy");
+
+
+            // Act
+            var result = await sut.EvaluateTypedAsync(context, CancellationToken.None);
+
+            // Assert
+            result.Status.ShouldBe(ResultStatus.Ok);
+            result.Value.ShouldBe(true);
+        }
+    }
+}
