ba7fbb25797b617dfe9796402aeed519d23f855b
Paul Deen <paul.deen@dutchworkz.nl>
2025-07-03 17:07:30 +0200

    Fixed typo

--- src/CrossCutting.Utilities.QueryEvaluator.CodeGeneration/CodeGenerationProviders/QueryEvaluatorCSharpClassBase.cs
+++ src/CrossCutting.Utilities.QueryEvaluator.CodeGeneration/CodeGenerationProviders/QueryEvaluatorCSharpClassBase.cs
@@ -11,7 +11,7 @@ public abstract class QueryEvaluatorCSharpClassBase(IPipelineService pipelineSer
     protected override Type EntityConcreteCollectionType => typeof(ReadOnlyValueCollection<>);
     protected override Type BuilderCollectionType => typeof(List<>);
     protected override string ProjectName => Constants.ProjectName;
-    protected override string CoreNamespace => Constants.Namespaces.UtilitiesQueryEvaluator; // standard implementation thinks we're using the project name concatenated with '.Domain'
+    protected override string CoreNamespace => Constants.Namespaces.UtilitiesQueryEvaluator; // standard implementation thinks we're using the project name concatenated with '.Core'
     protected override string BuilderAbstractionsNamespace => $"{RootNamespace}.Abstractions.Builders";
     protected override bool CopyAttributes => true;
     protected override bool CopyInterfaces => true;
