b1b2a4f3844c264880036ba5c83d255136cf3b9e
Paul Deen <paul.deen@dutchworkz.nl>
2025-07-29 19:25:13 +0200

    Put back old functionality in SingleEntityQueryParser

--- src/CrossCutting.Utilities.QueryEvaluator.QueryParsers/SingleEntityQueryParser.cs
+++ src/CrossCutting.Utilities.QueryEvaluator.QueryParsers/SingleEntityQueryParser.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using CrossCutting.Common;
 using CrossCutting.Common.Extensions;
 using CrossCutting.Utilities.QueryEvaluator.Abstractions;
 using CrossCutting.Utilities.QueryEvaluator.Abstractions.Builders;
 using CrossCutting.Utilities.QueryEvaluator.Abstractions.Builders.Extensions;
+using CrossCutting.Utilities.QueryEvaluator.Abstractions.Domains;
 using CrossCutting.Utilities.QueryEvaluator.Core.Builders.Conditions;
 using CrossCutting.Utilities.QueryEvaluator.Core.Builders.Expressions;
 
@@ -73,13 +75,18 @@ public class SingleEntityQueryParser<TQueryBuilder, TQueryExpressionBuilder> : I
             return default;
 #pragma warning restore S1168 // Empty arrays and collections should be returned instead of null
         }
+
+        var nextSearchCombination = Combination.And;
         var result = new List<IConditionBuilder>();
+
         for (int i = 0; i < items.Length; i += 4)
         {
             //verify that:
             //-items[i] needs to be a valid fieldname
             //-items[i + 1] needs to be a valid operator
             //-items[i + 3] needs to be a valid combination for the next condition
+            var startGroup = false;
+            var endGroup = false;
             var fieldName = items[i];
             var fieldValue = items[i + 2];
             var @operator = items[i + 1];
@@ -101,14 +108,23 @@ public class SingleEntityQueryParser<TQueryBuilder, TQueryExpressionBuilder> : I
                 return default;
             }
 
-            if (condition is ISingleExpressionContainerBuilder singleExpressionContainerBuilder)
-            {
-                singleExpressionContainerBuilder.WithFirstExpression(GetField(fieldName));
-            }
+            condition
+                .WithStartGroup(startGroup)
+                .WithEndGroup(endGroup)
+                .WithCombination(nextSearchCombination);
 
-            if (condition is IDoubleExpressionContainerBuilder doubleExpressionContainerBuilder)
+            (condition as ISingleExpressionContainerBuilder)?.WithFirstExpression(GetField(fieldName));
+            (condition as IDoubleExpressionContainerBuilder)?.WithSecondExpression(new LiteralExpressionBuilder(fieldValue));
+
+            if (items.Length > i + 3)
             {
-                doubleExpressionContainerBuilder.WithSecondExpression(new LiteralExpressionBuilder(fieldValue));
+                var combination = GetQueryCombination(items[i + 3]);
+                if (combination == null)
+                {
+                    return default;
+                }
+
+                nextSearchCombination = combination.Value;
             }
 
             result.Add(condition);
@@ -116,6 +132,14 @@ public class SingleEntityQueryParser<TQueryBuilder, TQueryExpressionBuilder> : I
 
         return result;
     }
+
+    private static Combination? GetQueryCombination(string combination)
+        => combination.ToUpper(CultureInfo.InvariantCulture) switch
+        {
+            "AND" => Combination.And,
+            "OR" => Combination.Or,
+            _ => null,// Unknown search combination
+        };
 
     private static PropertyNameExpressionBuilder GetField(string fieldName)
         => new PropertyNameExpressionBuilder().WithPropertyName(fieldName);
