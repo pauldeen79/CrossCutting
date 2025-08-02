3577032031398c87b317097d94cd9123ed43b781
Paul Deen <paul.deen@dutchworkz.nl>
2025-08-02 12:32:59 +0200

    Added In and NotIn operators

---
+++ src/CrossCutting.Utilities.Operators.Tests/InTests.cs
@@ -0,0 +1,64 @@
+﻿namespace CrossCutting.Utilities.Operators.Tests;
+
+public class InTests
+{
+    [Fact]
+    public void Returns_True_When_LeftValue_Is_Null_And_RightValue_Is_Enumerable_That_Contains_Null()
+    {
+        // Arrange
+        var leftValue = (object?)null;
+        var rightValue = new[] { "A", null };
+
+        // Act
+        var result = In.Evaluate(leftValue, rightValue, StringComparison.InvariantCulture);
+
+        // Assert
+        result.Status.ShouldBe(ResultStatus.Ok);
+        result.Value.ShouldBe(true);
+    }
+
+    [Fact]
+    public void Returns_True_When_LeftValue_Is_Null_And_RightValue_Is_Not_Enumerable_But_Is_Null()
+    {
+        // Arrange
+        var leftValue = (object?)null;
+        var rightValue = (object?)null;
+
+        // Act
+        var result = In.Evaluate(leftValue, rightValue, StringComparison.InvariantCulture);
+
+        // Assert
+        result.Status.ShouldBe(ResultStatus.Ok);
+        result.Value.ShouldBe(true);
+    }
+
+    [Fact]
+    public void Returns_True_When_LeftValue_Is_Part_Of_RightValue()
+    {
+        // Arrange
+        var leftValue = "A";
+        var rightValue = new[] { "A", "B" };
+
+        // Act
+        var result = In.Evaluate(leftValue, rightValue, StringComparison.InvariantCulture);
+
+        // Assert
+        result.Status.ShouldBe(ResultStatus.Ok);
+        result.Value.ShouldBe(true);
+    }
+
+    [Fact]
+    public void Returns_True_When_LeftValue_Is_Equal_To_RightValue()
+    {
+        // Arrange
+        var leftValue = "A";
+        var rightValue = "A";
+
+        // Act
+        var result = In.Evaluate(leftValue, rightValue, StringComparison.InvariantCulture);
+
+        // Assert
+        result.Status.ShouldBe(ResultStatus.Ok);
+        result.Value.ShouldBe(true);
+    }
+}
---
+++ src/CrossCutting.Utilities.Operators/In.cs
@@ -0,0 +1,27 @@
+﻿namespace CrossCutting.Utilities.Operators;
+
+public static class In
+{
+    public static Result<bool> Evaluate(object? leftValue, object? rightValue, StringComparison? stringComparison)
+        => Result.Success(leftValue is null
+            ? ValueContainsNull(rightValue)
+            : leftValue.In(stringComparison.GetValueOrDefault(StringComparison.CurrentCulture), GetValues(rightValue)));
+
+    private static bool ValueContainsNull(object? value)
+        => value is not string and IEnumerable enumerable
+            ? GetValues(enumerable).Contains(null)
+            : value is null;
+
+    private static IEnumerable<object?> GetValues(object? value)
+        => value is not string and IEnumerable enumerable
+            ? GetValues(enumerable)
+            : [value];
+
+    private static IEnumerable<object?> GetValues(IEnumerable enumerable)
+    {
+        foreach (var item in enumerable)
+        {
+            yield return item;
+        }
+    }
+}
