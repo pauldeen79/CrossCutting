4ebb13d4bf19eff520e32a6f07cb73be701e6c44
Paul Deen <paul.deen@dutchworkz.nl>
2025-08-23 17:17:33 +0200

    Refactored code a little bit

--- src/CrossCutting.Utilities.QueryEvaluator/Expressions/PropertyNameExpression.cs
+++ src/CrossCutting.Utilities.QueryEvaluator/Expressions/PropertyNameExpression.cs
@@ -6,20 +6,18 @@ public partial record PropertyNameExpression
     {
         context = ArgumentGuard.IsNotNull(context, nameof(context));
 
-        var valueResult = (await Expression.EvaluateAsync(context, token)
+        return (await Expression.EvaluateAsync(context, token)
             .ConfigureAwait(false))
-            .EnsureNotNull("Expression evaluation resulted in null");
-        if (!valueResult.IsSuccessful())
-        {
-            return valueResult;
-        }
+            .EnsureNotNull("Expression evaluation resulted in null")
+            .OnSuccess(valueResult =>
+            {
+                var property = valueResult.Value!.GetType().GetProperty(PropertyName, BindingFlags.Instance | BindingFlags.Public);
+                if (property is null)
+                {
+                    return Result.Invalid<object?>($"Type {valueResult.Value.GetType().FullName} does not contain property {PropertyName}");
+                }
 
-        var property = valueResult.Value!.GetType().GetProperty(PropertyName, BindingFlags.Instance | BindingFlags.Public);
-        if (property is null)
-        {
-            return Result.Invalid<object?>($"Type {valueResult.Value.GetType().FullName} does not contain property {PropertyName}");
-        }
-
-        return Result.WrapException(() => Result.Success<object?>(property.GetValue(valueResult.Value)));
+                return Result.WrapException(() => Result.Success<object?>(property.GetValue(valueResult.Value)));
+            });
     }
 }
