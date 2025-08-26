c2e1009313fe9d0bdfa7ea27a9882ed22d8d0434
Paul Deen <paul.deen@dutchworkz.nl>
2025-08-26 15:51:01 +0200

    Minor: Fixed typo in method name

--- src/CrossCutting.Utilities.QueryEvaluator.QueryProcessors.Sql/QueryProcessor.cs
+++ src/CrossCutting.Utilities.QueryEvaluator.QueryProcessors.Sql/QueryProcessor.cs
@@ -20,7 +20,7 @@ public class QueryProcessor : IQueryProcessor
     {
         query = ArgumentGuard.IsNotNull(query, nameof(query));
 
-        return await GetDatabaseCommandProvider<TResult>(query)
+        return await GetDatabaseEntityRetriever<TResult>(query)
             .OnSuccessAsync(async provider => await provider.FindManyAsync(CreateCommand(query, query.Limit.GetValueOrDefault()).DataCommand, cancellationToken).ConfigureAwait(false))
             .ConfigureAwait(false);
     }
@@ -29,7 +29,7 @@ public class QueryProcessor : IQueryProcessor
     {
         query = ArgumentGuard.IsNotNull(query, nameof(query));
 
-        return await GetDatabaseCommandProvider<TResult>(query)
+        return await GetDatabaseEntityRetriever<TResult>(query)
             .OnSuccessAsync(async provider =>
             {
                 var item = await provider.FindOneAsync(CreateCommand(query, 1).DataCommand, cancellationToken).ConfigureAwait(false);
@@ -44,7 +44,7 @@ public class QueryProcessor : IQueryProcessor
     {
         query = ArgumentGuard.IsNotNull(query, nameof(query));
 
-        return await GetDatabaseCommandProvider<TResult>(query)
+        return await GetDatabaseEntityRetriever<TResult>(query)
             .OnSuccessAsync(async provider => await provider.FindPagedAsync(CreateCommand(query, query.Limit.GetValueOrDefault()), cancellationToken).ConfigureAwait(false))
             .ConfigureAwait(false);
     }
@@ -52,7 +52,7 @@ public class QueryProcessor : IQueryProcessor
     private IPagedDatabaseCommand CreateCommand(IQuery query, int pageSize)
         => _pagedDatabaseCommandProvider.CreatePaged(query, DatabaseOperation.Select, query.Offset ?? 0, pageSize);
 
-    private Result<IDatabaseEntityRetriever<TResult>> GetDatabaseCommandProvider<TResult>(IQuery query) where TResult : class
+    private Result<IDatabaseEntityRetriever<TResult>> GetDatabaseEntityRetriever<TResult>(IQuery query) where TResult : class
     {
         foreach (var databaseEntityRetrieverProvider in _databaseEntityRetrieverProviders)
         {
