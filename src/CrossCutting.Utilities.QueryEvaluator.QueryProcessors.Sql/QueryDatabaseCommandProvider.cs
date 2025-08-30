2928daf711d3750ee3776b00bbdc9c58fa92d45c
Paul Deen <paul.deen@dutchworkz.nl>
2025-08-30 20:07:39 +0200

    Added missing null guard

--- src/CrossCutting.Utilities.QueryEvaluator.QueryProcessors.Sql/QueryDatabaseCommandProvider.cs
+++ src/CrossCutting.Utilities.QueryEvaluator.QueryProcessors.Sql/QueryDatabaseCommandProvider.cs
@@ -5,7 +5,11 @@ public class QueryDatabaseCommandProvider : IDatabaseCommandProvider<IQuery>
     private readonly IPagedDatabaseCommandProvider<IQuery> _pagedDatabaseCommandProvider;
 
     public QueryDatabaseCommandProvider(IPagedDatabaseCommandProvider<IQuery> pagedDatabaseCommandProvider)
-        => _pagedDatabaseCommandProvider = pagedDatabaseCommandProvider;
+    {
+        ArgumentGuard.IsNotNull(pagedDatabaseCommandProvider, nameof(pagedDatabaseCommandProvider));
+
+        _pagedDatabaseCommandProvider = pagedDatabaseCommandProvider;
+    }
 
     public IDatabaseCommand Create(IQuery source, DatabaseOperation operation)
     {
