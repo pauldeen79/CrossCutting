65a71039832aa59f8d553e733381a2e0f74e3875
Paul Deen <paul.deen@dutchworkz.nl>
2025-09-27 12:20:56 +0200

    Added tests for AllowedTypesAttribute

---
+++ src/CrossCutting.Common.Tests/DataAnnotations/AllowedTypesAttributeTests.cs
@@ -0,0 +1,53 @@
+﻿namespace CrossCutting.Common.Tests.DataAnnotations;
+
+public class AllowedTypesAttributeTests
+{
+    [Fact]
+    public void Is_Valid_On_Null_Value()
+    {
+        // Arrange
+        var sut = new MyClass { MyProperty = null! };
+        var results = new List<ValidationResult>();
+
+        // Act
+        _ = sut.TryValidate(results);
+
+        // Assert
+        results.ShouldBeEmpty();
+    }
+
+    [Fact]
+    public void Is_Valid_When_Value_Is_Of_Allowed_Type()
+    {
+        // Arrange
+        var sut = new MyClass { MyProperty = default(int) };
+        var results = new List<ValidationResult>();
+
+        // Act
+        _ = sut.TryValidate(results);
+
+        // Assert
+        results.ShouldBeEmpty();
+    }
+
+    [Fact]
+    public void Is_Invalid_When_Value_Is_Not_Of_Allowed_Type()
+    {
+        // Arrange
+        var sut = new MyClass { MyProperty = false };
+        var results = new List<ValidationResult>();
+
+        // Act
+        _ = sut.TryValidate(results);
+
+        // Assert
+        results.Count.ShouldBe(1);
+        results.Single().ErrorMessage.ShouldBe("Expression of type System.Boolean is not supported");
+    }
+
+    public class MyClass
+    {
+        [AllowedTypes([typeof(int), typeof(string)])]
+        public object? MyProperty { get; set; }
+    }
+}
