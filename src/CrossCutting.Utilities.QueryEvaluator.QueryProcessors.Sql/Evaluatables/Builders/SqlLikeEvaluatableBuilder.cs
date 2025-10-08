184464d50a73b193a674ea201362cb41f7eb929b
Paul Deen <paul.deen@dutchworkz.nl>
2025-10-08 20:48:47 +0200

    This code can now be removed

--- src/CrossCutting.Utilities.QueryEvaluator.QueryProcessors.Sql/Evaluatables/Builders/SqlLikeEvaluatableBuilder.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CrossCutting.Utilities.QueryEvaluator.QueryProcessors.Sql.Evaluatables.Builders;
-
-public partial class SqlLikeEvaluatableBuilder : IEvaluatableBuilder
-{
-    IEvaluatable IBuilder<IEvaluatable>.Build()
-        => new SqlLikeEvaluatable(SourceExpression?.Build()!, FormatString);
-}
--- src/CrossCutting.Utilities.QueryEvaluator/Builders/Evaluatables/PropertyNameEvaluatableBuilder.cs
+++ src/CrossCutting.Utilities.QueryEvaluator/Builders/Evaluatables/PropertyNameEvaluatableBuilder.cs
@@ -2,9 +2,6 @@
 
 public partial class PropertyNameEvaluatableBuilder : IEvaluatableBuilder
 {
-    IEvaluatable IBuilder<IEvaluatable>.Build()
-        => new PropertyNameEvaluatable(SourceExpression?.Build()!, PropertyName);
-
     partial void SetDefaultValues()
     {
         SourceExpression = new ContextEvaluatableBuilder();
