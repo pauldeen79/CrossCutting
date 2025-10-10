5da528da5800543ab5e333abd4416fcc2bc01b70
Paul Deen <paul.deen@dutchworkz.nl>
2025-10-10 18:16:22 +0200

    Replaced EvaluateAsync implementation to make clear that this is not used

--- src/CrossCutting.Utilities.QueryEvaluator.QueryProcessors.Sql/Evaluatables/SqlLikeEvaluatable.cs
+++ src/CrossCutting.Utilities.QueryEvaluator.QueryProcessors.Sql/Evaluatables/SqlLikeEvaluatable.cs
@@ -3,5 +3,5 @@
 public partial record SqlLikeEvaluatable
 {
     public override Task<Result<object?>> EvaluateAsync(ExpressionEvaluatorContext context, CancellationToken token)
-        => SourceExpression.EvaluateAsync(context, token);
+        => Task.FromResult(Result.NotImplemented<object?>($"{nameof(SqlLikeEvaluatable)} can't be evaluated directly"));
 }
