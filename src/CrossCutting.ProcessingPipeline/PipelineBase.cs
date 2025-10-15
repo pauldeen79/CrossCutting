4f06c9ce5934b2d0a3d34e2b616c8c6987d1d1a0
Paul Deen <paul.deen@dutchworkz.nl>
2025-10-15 14:40:16 +0200

    Refactor: Simplified ProcessingPipeline

--- src/CrossCutting.ProcessingPipeline/PipelineBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CrossCutting.ProcessingPipeline;
-
-public class PipelineBase<TRequest>(IEnumerable<IPipelineComponent<TRequest>>? features) : AbstractPipelineBase<IPipelineComponent<TRequest>>(features)
-{
-}
-
-public class PipelineBase<TRequest, TResponse>(IEnumerable<IPipelineComponent<TRequest, TResponse>>? features) : AbstractPipelineBase<IPipelineComponent<TRequest, TResponse>>(features)
-{
-}
