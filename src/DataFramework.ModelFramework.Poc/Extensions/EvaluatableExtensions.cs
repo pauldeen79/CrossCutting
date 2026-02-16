ee47231885eff25402766af00cfb78663bc5eb2e
Paul Deen <pauldeen@gmx.li>
2026-02-16 11:09:22 +0100

    Let's use LINQ

--- src/DataFramework.ModelFramework.Poc/Extensions/EvaluatableExtensions.cs
+++ src/DataFramework.ModelFramework.Poc/Extensions/EvaluatableExtensions.cs
@@ -9,28 +9,17 @@ namespace DataFramework.ModelFramework.Poc.Extensions;
 
 public static class EvaluatableExtensions
 {
-        public static bool IsValidExpression(this IEvaluatable evaluatable, IEnumerable<string> validFieldNames)
+    public static bool IsValidExpression(this IEvaluatable evaluatable, IEnumerable<string> validFieldNames)
+         => evaluatable.GetContainedEvaluatables(true).All(x => IsValidExpressionRecursive(x, validFieldNames));
+
+    private static bool IsValidExpressionRecursive(IEvaluatable evaluatable, IEnumerable<string> validFieldNames)
+    {
+        if (evaluatable is PropertyNameEvaluatable propertyNameEvaluatable)
         {
-            foreach (var childEvaluatable in evaluatable.GetContainedEvaluatables(true))
-            {
-                if (!IsValidExpressionCore(childEvaluatable, validFieldNames))
-                {
-                    return false;
-                }
-            }
-            return true;
+            return validFieldNames.Any(s => s.Equals(propertyNameEvaluatable.PropertyName, StringComparison.Ordinal));
         }
 
-        private static bool IsValidExpressionCore(IEvaluatable evaluatable, IEnumerable<string> validFieldNames)
-        {
-            if (evaluatable is PropertyNameEvaluatable propertyNameEvaluatable)
-            {
-                var result = false;
-
-                return result || validFieldNames.Any(s => s.Equals(propertyNameEvaluatable.PropertyName, StringComparison.Ordinal));
-            }
-
-            // You might want to validate the expression to prevent sql injection (unless you can only create query expressions in code)
-            return evaluatable.GetType().Assembly.FullName.StartsWith("CrossCutting.Utilities.QueryEvaluator");
-        }
+        // You might want to validate the expression to prevent sql injection (unless you can only create query expressions in code)
+        return evaluatable.GetType().Assembly.FullName.StartsWith("CrossCutting.Utilities.QueryEvaluator");
+    }
 }
