29a179316c5b885932207558292e73989bdfa5f9
Paul Deen <pauldeen@gmx.li>
2026-03-01 17:22:44 +0100

    Fixed last TODO

--- src/CrossCutting.Utilities.QueryEvaluator/Builders/Extensions/QueryBuilderExtensions.cs
+++ src/CrossCutting.Utilities.QueryEvaluator/Builders/Extensions/QueryBuilderExtensions.cs
@@ -6,11 +6,6 @@ public static partial class QueryBuilderExtensions
         where T : IQueryBuilder
         => new ComposableEvaluatableFieldNameBuilderWrapper<T>(instance, fieldName);
 
-    //TODO: Review why this causes an ambigious reference exception in tests
-    // public static ComposableEvaluatableExpressionBuilderWrapper<T> Where<T>(this T instance, IEvaluatableBuilder expression)
-    //     where T : IQueryBuilder
-    //     => new ComposableEvaluatableExpressionBuilderWrapper<T>(instance, expression);
-
     public static ComposableEvaluatableFieldNameBuilderWrapper<T> Or<T>(this T instance, string fieldName)
         where T : IQueryBuilder
         => new ComposableEvaluatableFieldNameBuilderWrapper<T>(instance, fieldName, Combination.Or);
