da13263599d2e6c20073d97921c7bbf8ee8c95a1
Paul Deen <pauldeen@gmx.li>
2026-03-03 19:44:44 +0100

    Little refactoring: Method should throw if the argument is not of the right type

--- src/CrossCutting.Utilities.QueryEvaluator.Abstractions/Builders/Extensions/ConditionBuilderExtensions.cs
+++ src/CrossCutting.Utilities.QueryEvaluator.Abstractions/Builders/Extensions/ConditionBuilderExtensions.cs
@@ -4,14 +4,24 @@ public static partial class ConditionBuilderExtensions
 {
     public static IConditionBuilder WithLeftExpression(this IConditionBuilder instance, IEvaluatableBuilder expression)
     {
-        (instance as ISourceExpressionContainerBuilder)?.WithSourceExpression(expression);
+        if (instance is not ISourceExpressionContainerBuilder sourceExpressionContainerBuilder)
+        {
+            throw new ArgumentException($"Condition {instance.GetType().FullName} is not assignable to {nameof(ICompareExpressionContainerBuilder)}");
+        }
+
+        sourceExpressionContainerBuilder.WithSourceExpression(expression);
         
         return instance;
     }
 
     public static IConditionBuilder WithRightExpression(this IConditionBuilder instance, IEvaluatableBuilder expression)
     {
-        (instance as ICompareExpressionContainerBuilder)?.WithCompareExpression(expression);
+        if (instance is not ICompareExpressionContainerBuilder compareExpressionContainerBuilder)
+        {
+            throw new ArgumentException($"Condition {instance.GetType().FullName} is not assignable to {nameof(ICompareExpressionContainerBuilder)}");
+        }
+        
+        compareExpressionContainerBuilder.WithCompareExpression(expression);
         
         return instance;
     }
