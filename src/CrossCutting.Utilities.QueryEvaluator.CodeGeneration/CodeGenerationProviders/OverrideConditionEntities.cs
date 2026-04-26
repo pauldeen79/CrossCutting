3aa65e79caf43a1cfe943994c7abb867165b2de7
Paul Deen <pauldeen@gmx.li>
2026-04-26 15:54:27 +0200

    Fixed code generation for QueryEvaluator

--- src/CrossCutting.Utilities.QueryEvaluator.CodeGeneration/CodeGenerationProviders/OverrideConditionEntities.cs
+++ src/CrossCutting.Utilities.QueryEvaluator.CodeGeneration/CodeGenerationProviders/OverrideConditionEntities.cs
@@ -7,6 +7,8 @@ public class OverrideConditionEntities(ICommandService commandService) : QueryEv
 
     protected override bool EnableEntityInheritance => true;
     protected override Task<Result<TypeBase>> GetBaseClassAsync() => CreateBaseClassAsync(typeof(IConditionBase), $"{Constants.Namespaces.UtilitiesQueryEvaluator}.Core");
+    //This is kind of a hack, because the name ToTypedBuilder is already taken. So we need to use a different name.
+    protected override string ToTypedBuilderFormatString => "ToTypedBuilderCore";
 
     public override Task<Result<IEnumerable<TypeBase>>> GetModelAsync(CancellationToken token)
         => GetEntitiesAsync(GetOverrideModelsAsync(typeof(IConditionBase)), $"{Constants.Namespaces.UtilitiesQueryEvaluator}.Core.Conditions");
