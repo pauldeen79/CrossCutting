8acfa7d599a023d5b1189f5f79b9d0aa698e3880
Paul Deen <pauldeen@gmx.li>
2026-05-02 20:10:10 +0200

    Let's use the In operator from the In function

--- src/CrossCutting.Utilities.ExpressionEvaluator/Functions/InFunction.cs
+++ src/CrossCutting.Utilities.ExpressionEvaluator/Functions/InFunction.cs
@@ -26,6 +26,6 @@ public class InFunction : IFunction
             rightValues.Add(result.Value);
         }
 
-        return leftValueResult.Value.In(context.Context.Settings.StringComparison, rightValues);
+        return In.Evaluate(leftValueResult.Value, rightValues, context.Context.Settings.StringComparison);
     }
 }
